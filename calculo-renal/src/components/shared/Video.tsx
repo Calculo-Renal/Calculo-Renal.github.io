361f04b0950e482a125499ed5b3580707685503e
JeanC4rlo <jeancs.santos1@gmail.com>
2026-04-24 20:43:57 -0300

    Corrigindo quebra de layout do LaTeX

--- calculo-renal/src/components/shared/Video.tsx
+++ calculo-renal/src/components/shared/Video.tsx
@@ -10,8 +10,6 @@ export default function Video({ src, loop }: VideoProps) {
             loop={loop}
             controls
             style={{
-                width: "100%",
-                height: "100%",
                 objectFit: "cover",
                 imageRendering: "smooth"
             }}
