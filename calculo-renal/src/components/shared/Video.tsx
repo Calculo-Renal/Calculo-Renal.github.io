361f2c1becebbc4a26cd1653b564b0ca876bd79a
JeanC4rlo <jeancs.santos1@gmail.com>
2026-04-24 19:53:36 -0300

    Adicionando vídeo de limites

--- calculo-renal/src/components/shared/Video.tsx
+++ calculo-renal/src/components/shared/Video.tsx
@@ -5,6 +5,16 @@ interface VideoProps {
 
 export default function Video({ src, loop }: VideoProps) {
     return (
-        <video src={src} loop={loop} controls></video>
+        <video
+            src={src}
+            loop={loop}
+            controls
+            style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                imageRendering: "smooth"
+            }}
+        />
     )
 }
